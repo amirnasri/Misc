e5d93e6168a32e206eb1eecc919ec4646a54cd01
amirnasri <amnasri@gmail.com>
2015-03-31 01:13:37 -0700

    CCI_Q1_3

--- src/CCI_Q1_3.java
+++ src/CCI_Q1_3.java
@@ -5,20 +5,46 @@
 	An extra copy of the array is not.
 	FOLLOW UP
 	Write the test cases for this method.
+	
+	Better solution: use quick sort to sort O(n log(n)) in place and then remove duplicates in O(n).
  */
 public class CCI_Q1_3 {
 	
 	// CCI solution
 	static int remove_duplicate1(int[] array) {
-		return 0;
+		if (array == null)
+			return -1;
+		if (array.length == 0)
+			return 0;
+		
+		int tail = 1;
+		int j;
+		
+		for (int i = 1; i < array.length; i++) {
+			for (j = 0; j < tail; j++) {
+				if (array[i] == array[j]) {
+					break;
+				}
+			}
+
+			if (j == tail) {
+				array[tail] = array[i];
+				tail++;
+			}
+		}
+		return tail;
 	}
 	
 	/*
-	 * Use a double loop to look for duplicates. When a dublicate is found, move it to the end of the array
+	 * Use a double loop to look for duplicates. When a duplicate is found, move it to the end of the array
 	 * and reduce the length by 1
 	 * complexity = O(n^2)
 	 */
 	static int remove_duplicate(int[] array) {
+		
+		if (array == null)
+			return -1;
+		
 		int len = array.length;
 		int i = 0, j;
 		
@@ -41,10 +67,11 @@ public class CCI_Q1_3 {
 	}
 	
 	public static void main(String[] args) {
-		//int[] array = new int[] {1, 2, 7, 3, 4, 2, 6};
+		//int[] array = new int[] {1, 2, 7, 1, 4, 1, 6};
 		//int[] array = new int[] {1, 2, 3, 4, 5, 6, 7};
-		int[] array = new int[] {2, 1};
-		int new_len = remove_duplicate(array);
+		int[] array = new int[] {1, 1, 1, 2, 2, 2, 2};
+		//int[] array = null;
+		int new_len = remove_duplicate1(array);
 		System.out.println(new_len);
 		for (int i = 0; i < new_len; i++) {
 			System.out.print(array[i] + " ");
