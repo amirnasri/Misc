a5494a313b3642c23fd14da554ba9f7debf97a18
amirnasri <amnasri@gmail.com>
2015-04-06 01:03:29 -0700

    Create CCI_Q1_1.java

---
+++ src/CCI_Q1_1.java
@@ -0,0 +1,61 @@
+import java.util.HashSet;
+
+/*
+ * Implement an algorithm to determine if a string has all unique characters. What if
+ * you can not use additional data structures?
+ */
+		
+public class CCI_Q1_1 {
+	
+	private static boolean is_char_unique(String s) {
+		HashSet<Character> hash_set = new HashSet<>();
+		
+		for (int i = 0; i < s.length(); i++) {
+			if (!hash_set.add(s.charAt(i)))
+				return false;	
+		}
+		return true;
+	}
+
+	private static boolean is_char_unique1(String s) {
+		long v = 0;
+		
+		for (int i = 0; i < s.length(); i++) {
+			byte shift = (byte) (s.charAt(i)-'0');
+			System.out.println(shift);
+			if ((v & (1 << shift)) != 0) {
+				return false;
+			}
+			else {
+				v |= (1 << shift);
+			}
+		}
+		return true;
+	}
+	
+	private static boolean is_char_unique2(String s) {
+		boolean[] array = new boolean[1<<16];
+		for (int i = 0; i < s.length(); i++) {
+			if (array[s.charAt(i)])
+				return false;
+			else
+				array[s.charAt(i)] = true;
+		}
+		return true;
+	}
+	
+	public static void main(String[] args) {
+		String s = new String("abcfepnwvkz1c");
+		System.out.println(is_char_unique2(s));
+		int[] a = new int[10];
+		System.out.println(a);
+		
+
+	}
+}
+“In a collection of strings, output the string that occurs most often.”
+“find out all the pairs in an array that adds up to a certain number”
+Given a set of number ranges, i.e. [1,5], [3,6], [7,9], write a program to merge overlapping number ranges. In the above example, the result should be [1,6], [7,9].”
+“Check if all the leaves are at the same level in a binary tree”
+“sort a list of words alpha-numerically.”
+write a method that returns the most common character in a string.
