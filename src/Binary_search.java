91fe91434084d2cff287eb640b7636bb73f61195
amirnasri <amnasri@gmail.com>
2015-08-30 20:59:18 -0700

    Update Binary_search.java

--- src/Binary_search.java
+++ src/Binary_search.java
@@ -1,7 +1,7 @@
 
 
 public class Binary_search {
-	
+	/*
 	static int binary_search(int key, int[] array) {
 		int low = 0; 
 		int high = array.length - 1;
@@ -22,9 +22,45 @@ public class Binary_search {
 		}
 		return mid;
 	}
-
-	public static void main(String[] args) {
-		int[] array = new int[] {1, 4, 7, 12};
-		System.out.println(binary_search(14, array));
+	*/
+	
+	// New (better) implementation
+	static <T extends Comparable> int binary_search(T key, T[] arr, int low, int high) {
+		int m;
+		T middle;
+		while (low < high) {
+			m = (low + high)/2;
+			middle = arr[m];
+			if (key == middle)
+				return m;
+			else if (key.compareTo(middle) < 0)
+				high = m;
+			else
+				low = m + 1;
+		}
+		return low;
 	}
+	
+	public static void main(String[] args) {
+		final int array_length = 5;
+		Integer[] arr = new Integer[array_length];
+		Random rg = new Random();
+		for (int i = 0; i < array_length; i++) {
+			arr[i] = rg.nextInt(2 * array_length);
+		}
+		
+		Arrays.sort(arr);
+		
+		int index = binary_search(8, arr, 0, arr.length);
+		
+		for (int i: arr) {
+			System.out.printf("%-6d", i);
+		}
+		System.out.println();
+		
+		System.out.println(index);
+		System.out.println(String.format("%" + (int) (index *6 + 1) +"s", "-"));
+		
+	}
+
 }
