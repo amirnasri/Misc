04c748571866ec09f992f2dfa868d8b2a99146ea
Amir <amir@fortinet>
2015-08-31 10:53:37 -0700

    -

--- src/Overlapping_intervals.java
+++ src/Overlapping_intervals.java
@@ -1,14 +1,21 @@
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+
 
 public class Overlapping_intervals {
 	
-	static final int m = 5;
+	static final int m = 6;
 	static Interval[] intervs;
 	
-	class Interval {
+	static class Interval {
 		int l, h;
 		Interval(int l, int h) {
 			this.l = l;
 			this.h = h;
+		}
+		void print() {
+			System.out.println("[" + l + ", " + h + "]");
 		}
 	}
 	static boolean merge(Interval i1, Interval i2, Interval i) {
@@ -25,17 +32,40 @@ public class Overlapping_intervals {
 	public static void main(String[] args) {
 		
 		intervs = new Interval[m];
+		intervs[0] = new Interval(0, 2);
+		intervs[1] = new Interval(1, 4);
+		intervs[2] = new Interval(10, 11);
+		intervs[3] = new Interval(7, 9);
+		intervs[4] = new Interval(10, 14);
+		intervs[5] = new Interval(3, 6);
+
+		Arrays.sort(intervs, new Comparator<Interval>() {
+			@Override
+			public int compare(Interval i1, Interval i2) {
+				if  (i1.l < i2.l)
+					return -1;
+				else if (i1.l > i2.l)
+					return 1;
+				else 
+					return 0;
+			}
+		});
 		Interval curr = intervs[0];
-		Interval next, merged = new Interval(0, 0);
+		Interval next;
 		for (int i = 1; i < m; i++) {
 			next = intervs[i];
-			if (merge(curr, next, merged))
-				curr = merged;
-			else {
+			if (curr.h < next.l) {
 				curr.print();
 				curr = next;
 			}
+			else {
+				curr.h = next.h;
+			}
 		}
+		
+		curr.print();
+		
+		
 		
 		/*
 		while (i < m) {
