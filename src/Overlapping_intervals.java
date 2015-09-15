0023d9b2aebbeac153d1f62755288254338b20f7
Amir <amir@fortinet>
2015-09-15 14:30:18 -0700

    -

--- src/Overlapping_intervals.java
+++ src/Overlapping_intervals.java
@@ -1,32 +1,92 @@
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.Comparator;
+import java.util.HashMap;
+import java.util.ArrayList;
 
-
-public class Overlapping_intervals {
+class Overlapping_intervals {
 	
-	static final int m = 6;
-	static Interval[] intervs;
+	private static final int m = 6;
+	private static Interval[] intervs;
 	
 	static class Interval {
-		int l, h;
+		private int l, h;
 		Interval(int l, int h) {
 			this.l = l;
 			this.h = h;
+		}
+		Interval(Interval i) {
+			this(i.l, i.h);
+		}
+		Interval() {
+			this(0, 0);
 		}
 		void print() {
 			System.out.println("[" + l + ", " + h + "]");
 		}
 	}
-	static boolean merge(Interval i1, Interval i2, Interval i) {
-		i.l = i1.l;
-		if (i1.h < i2.l)
-			return false;
-		if (i1.h < i2.h)
-			i.h = i2.h;
-		else
-			i.h = i1.h;
-		return true;
+
+	
+	static void find_overlaps(Interval[] intervs) {
+		Arrays.sort(intervs, new Comparator<Interval>() {
+			@Override
+			public int compare(Interval i1, Interval i2) {
+				if  (i1.l < i2.l)
+					return -1;
+				else if (i1.l > i2.l)
+					return 1;
+				else 
+					return 0;
+			}
+		});
+		Interval curr = new Interval(intervs[0]);
+		Interval next;
+		for (int i = 1; i < m; i++) {
+			next = new Interval(intervs[i]);
+			if (curr.h < next.l) {
+				curr.print();
+				curr = next;
+			}
+			else {
+				curr.h = next.h;
+			}
+		}
+		
+		curr.print();
+	}
+	
+	// A better approad inspired by LaserShooting problem
+	static void find_overlaps1(Interval[] intervals) {
+		
+		ArrayList<Integer[]> points = new ArrayList<>();
+		for (Interval i: intervals) {
+			points.add(new Integer[]{i.l, 1});
+			points.add(new Integer[]{i.h, -1});
+		}
+
+		Collections.sort(points, new Comparator<Integer[]>() {
+			@Override
+			public int compare(Integer[] i1, Integer[] i2) {
+				return Integer.compare(i1[0], i2[0]);
+			}
+		});
+		
+	
+		int open_intervs = 0;
+		Interval curr = new Interval(0, 0);
+		for (int i = 0; i < points.size(); i++) {
+			int new_point = points.get(i)[0];
+			if (open_intervs == 0) {
+				curr.l = new_point;
+			}
+			open_intervs += points.get(i)[1];
+			System.out.printf("(%d, %d)   ", new_point, open_intervs);
+			if (open_intervs == 0) {
+				curr.h = new_point;
+				//curr.print();
+			}
+		}
+		System.out.println();
 	}
 	
 	public static void main(String[] args) {
@@ -39,56 +99,11 @@ public class Overlapping_intervals {
 		intervs[4] = new Interval(10, 14);
 		intervs[5] = new Interval(3, 6);
 
-		Arrays.sort(intervs, new Comparator<Interval>() {
-			@Override
-			public int compare(Interval i1, Interval i2) {
-				if  (i1.l < i2.l)
-					return -1;
-				else if (i1.l > i2.l)
-					return 1;
-				else 
-					return 0;
-			}
-		});
-		Interval curr = intervs[0];
-		Interval next;
-		for (int i = 1; i < m; i++) {
-			next = intervs[i];
-			if (curr.h < next.l) {
-				curr.print();
-				curr = next;
-			}
-			else {
-				curr.h = next.h;
-			}
-		}
-		
-		curr.print();
+		find_overlaps(intervs);
+		System.out.println();
+		find_overlaps1(intervs);
 		
 		
-		
-		/*
-		while (i < m) {
-			int l = intervs[i][0];
-			int h = intervs[i][1];
-			
-			i++;
-			while (true) {
-				int new_l = intervs[i][0];
-				int new_h = intervs[i][1];
-				if (i >= m) {
-					System.out.print("[ " + l + ", " + h + "]");
-					break;
-				}
-				if (new_h < h)
-					continue;
-				else if (new_h > h && new_l < h)
-					h = new_h;
-				System.out.print("[ " + l + ", " + h + "]");
-				break;
-			}
-		}
-		*/
 	}
 
 }
--- src/Sort_string_array.java
+++ src/Sort_string_array.java
@@ -1,6 +1,12 @@
 import java.util.Arrays;
 import java.util.Comparator;
 
+/*
+ * Compare to string arrays using only character comparison. 
+ * Note: Natural ordering for char and String means "A" < "B" < "a". So all upper case words will be before the lower case ones.
+ * To fix this we can modify the char or String comparator to compare the lower case items and return the result if they are not equal. 
+ * If the items are equal in lower case, return the natural order.
+ */
 
 public class Sort_string_array {
 	
@@ -17,8 +23,13 @@ public class Sort_string_array {
 				return -1;
 			if (s2.length() < pos + 1)
 				return 1;
-				
-			return (Character.compare(s1.charAt(pos), s2.charAt(pos)));
+			char c1 = s1.charAt(pos);
+			char c2 = s2.charAt(pos);
+			int cmp = Character.compare(Character.toLowerCase(c1), Character.toLowerCase(c2));
+			
+			if (cmp != 0)
+				return cmp;
+			return Character.compare(c1, c2);
 			
 		}
 	}
@@ -39,17 +50,36 @@ public class Sort_string_array {
 	}
 	
 	public static void main(String[] args) {
-		String[] str_array = new String[] {"ac", "ab", "bfc", "ba"};
+		//String[] str_array = new String[] {"ac", "ab", "bfc", "ba"};
+		String[] str_array = new String[] {"development", "aBc", "Development", "aa", "AA", "Aa"};
 		
 		for (String s: str_array) 
 			System.out.print(s + "  ");
 		System.out.println();
 		
 		//sort(str_array);
-		Arrays.sort(str_array);
+		//Arrays.sort(str_array);
+		
+		
+		Arrays.sort(str_array, new Comparator<String>() {
+			@Override
+			public int compare(String s1, String s2) {
+				int cmp = s1.compareToIgnoreCase(s2);
+				
+				if (cmp != 0)
+					return cmp;
+				else
+					return s1.compareTo(s2);
+			}
+
+		});
+		
 		
 		for (String s: str_array) 
 			System.out.print(s + "  ");
 		System.out.println();
+		
+		
+		
 	}
 }
