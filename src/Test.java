0c0f7765530b64acc5d7df26702c8f68e7900fdb
amirnasri <amnasri@gmail.com>
2015-09-17 00:57:02 -0700

    -

---
+++ src/Test.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class Test {
+	private final static List a;
+	
+	static {
+		a = new ArrayList<>();	
+	}
+	
+	Test() {
+	
+	}
+	
+	void test_method() {
+	}
+	
+	public static void main(String[] args) {
+		System.out.println("d");
+	}
+}
